c380d39db359a837eab79800150007850c0fc4bd
Jorge Simões <89870657+Djosh193@users.noreply.github.com>
2026-03-21 18:04:15 +0000

    adiciona os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>O kra é um monstro xulinhos!!! a cabeça tá abrindo pras possibilidades, agora algumas coisas estão fazendo mais sentido.</h1>
+  return (
+    <h1>
+      O kra é um monstro xulinhos!!! a cabeça tá abrindo pras possibilidades,
+      agora algumas coisas estão fazendo mais sentido.
+    </h1>
+  );
 }
 
 export default Home;
